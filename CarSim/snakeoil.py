60345b29c5ecbc42e03e821008d3c449b69c9dce
MarcoFrancescoMerola <merolamma@gmail.com>
2021-08-21 12:23:55 +0200

    nodamage param removed

--- CarSim/snakeoil.py
+++ CarSim/snakeoil.py
@@ -557,7 +557,7 @@ def drive_example(c):
 # ================ MAIN ================
 if __name__ == "__main__":
     C= Client()
-    for step in xrange(C.maxSteps,0,-1):
+    for step in range(C.maxSteps,0,-1):
         C.get_servers_input()
         drive_example(C)
         C.respond_to_server()
